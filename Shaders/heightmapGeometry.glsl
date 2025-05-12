f4e5e8212cb20e77bda7c587e4dcdff2b5841d2f
David Tarifa <92268806+Bey0ndTheSky@users.noreply.github.com>
2025-05-12 20:57:30 +0100

    Cut down on geom shader
    TO DO: Find a way to do grass regardless of vertex

--- Shaders/heightmapGeometry.glsl
+++ Shaders/heightmapGeometry.glsl
@@ -75,107 +75,105 @@ void main(void) {
     EmitVertex();
     EndPrimitive();
 	
-	for (int i = 0; i < 3; ++i) { 
-		
-		vec4 colourBladeTop = useGrassColour ? colourTop : IN[i].colour;
-		vec4 colourBladeBase = useGrassColour ? colourBase : IN[i].colour;
+	int i = 0;
+	vec4 colourBladeTop = useGrassColour ? colourTop : IN[i].colour;
+	vec4 colourBladeBase = useGrassColour ? colourBase : IN[i].colour;
+
+	vec3 heightNormal = normalize(IN[i].normal);
 	
-		vec3 heightNormal = normalize(IN[i].normal);
-		
-		float randValue = rand(IN[i].worldPos);
-		float randomAngle = randValue * 2.0 * 3.14159;
-		float c = cos(randomAngle);
-		float s = sin(randomAngle);
+	float randValue = rand(IN[i].worldPos);
+	float randomAngle = randValue * 2.0 * 3.14159;
+	float c = cos(randomAngle);
+	float s = sin(randomAngle);
+
+	mat3 rotationYMatrix = mat3(
+		c, 0.0, s,
+		0.0, 1.0, 0.0,
+		-s, 0.0, c
+	);
 	
-		mat3 rotationYMatrix = mat3(
-			c, 0.0, s,
-			0.0, 1.0, 0.0,
-			-s, 0.0, c
-        );
-		
-		randomAngle = randValue * 0.25 * 3.14159;
-		c = cos(randomAngle);
-		s = sin(randomAngle);
-		mat3 rotationXMatrix = mat3(
-            1.0, 0.0, 0.0,
-			0.0, c, -s,
-			0.0, s, c
-        );
-		vec3 randomTangent = normalize(rotationYMatrix * rotationXMatrix * IN[i].tangent);
-		
-		float width = mix(bladeWidth * 0.5, bladeWidth * 2, rand(IN[i].worldPos.xzy));
-		float height = mix(grassHeight * 0.5, grassHeight * 2, rand(IN[i].worldPos.zxy));
-		
-        vec3 baseLeft = IN[i].worldPos - randomTangent * (width / 2.0);  // Left side of the blade
-        vec3 baseRight = IN[i].worldPos + randomTangent * (width / 2.0);  // Right side of the blade
-        
-        // Calculate the top positions of the grass blade (using the normal to lift it)
-        vec3 top = IN[i].worldPos + normalize(rotationYMatrix * rotationXMatrix * heightNormal) * height;
-		OUT.texCoord = IN[i].texCoord;
-		
-		vec4 windSample = (texture(windMap, OUT.texCoord / 50.0 + windTraslate) * 2 - 1);
-		vec3 wind = normalize(vec3(windSample.r, 0.0, windSample.g));
-		vec3 rotAxis = normalize(cross(wind, heightNormal));
-		float angle = acos(dot(wind, heightNormal)) * windStrength;
-		angle = clamp(angle + randomAngle, -3.14159 / 3.0, 3.14159 / 3.0);
-		
-		mat3 windRotation = mat3(
-        cos(angle) + rotAxis.x * rotAxis.x * (1.0 - cos(angle)),
-        rotAxis.x * rotAxis.y * (1.0 - cos(angle)) - rotAxis.z * sin(angle),
-        rotAxis.x * rotAxis.z * (1.0 - cos(angle)) + rotAxis.y * sin(angle),
+	randomAngle = randValue * 0.25 * 3.14159;
+	c = cos(randomAngle);
+	s = sin(randomAngle);
+	mat3 rotationXMatrix = mat3(
+		1.0, 0.0, 0.0,
+		0.0, c, -s,
+		0.0, s, c
+	);
+	vec3 randomTangent = normalize(rotationYMatrix * rotationXMatrix * IN[i].tangent);
+	
+	float width = mix(bladeWidth * 0.5, bladeWidth * 2, rand(IN[i].worldPos.xzy));
+	float height = mix(grassHeight * 0.5, grassHeight * 2, rand(IN[i].worldPos.zxy));
+	
+	vec3 baseLeft = IN[i].worldPos - randomTangent * (width / 2.0);  // Left side of the blade
+	vec3 baseRight = IN[i].worldPos + randomTangent * (width / 2.0);  // Right side of the blade
+	
+	// Calculate the top positions of the grass blade (using the normal to lift it)
+	vec3 top = IN[i].worldPos + normalize(rotationYMatrix * rotationXMatrix * heightNormal) * height;
+	OUT.texCoord = IN[i].texCoord;
+	
+	vec4 windSample = (texture(windMap, OUT.texCoord / 50.0 + windTraslate) * 2 - 1);
+	vec3 wind = normalize(vec3(windSample.r, 0.0, windSample.g));
+	vec3 rotAxis = normalize(cross(wind, heightNormal));
+	float angle = acos(dot(wind, heightNormal)) * windStrength;
+	angle = clamp(angle + randomAngle, -3.14159 / 3.0, 3.14159 / 3.0);
+	
+	mat3 windRotation = mat3(
+	cos(angle) + rotAxis.x * rotAxis.x * (1.0 - cos(angle)),
+	rotAxis.x * rotAxis.y * (1.0 - cos(angle)) - rotAxis.z * sin(angle),
+	rotAxis.x * rotAxis.z * (1.0 - cos(angle)) + rotAxis.y * sin(angle),
 
-        rotAxis.y * rotAxis.x * (1.0 - cos(angle)) + rotAxis.z * sin(angle),
-        cos(angle) + rotAxis.y * rotAxis.y * (1.0 - cos(angle)),
-        rotAxis.y * rotAxis.z * (1.0 - cos(angle)) - rotAxis.x * sin(angle),
+	rotAxis.y * rotAxis.x * (1.0 - cos(angle)) + rotAxis.z * sin(angle),
+	cos(angle) + rotAxis.y * rotAxis.y * (1.0 - cos(angle)),
+	rotAxis.y * rotAxis.z * (1.0 - cos(angle)) - rotAxis.x * sin(angle),
 
-        rotAxis.z * rotAxis.x * (1.0 - cos(angle)) - rotAxis.y * sin(angle),
-        rotAxis.z * rotAxis.y * (1.0 - cos(angle)) + rotAxis.x * sin(angle),
-        cos(angle) + rotAxis.z * rotAxis.z * (1.0 - cos(angle))
-    );
-		top = IN[i].worldPos + windRotation * (top - IN[i].worldPos);
+	rotAxis.z * rotAxis.x * (1.0 - cos(angle)) - rotAxis.y * sin(angle),
+	rotAxis.z * rotAxis.y * (1.0 - cos(angle)) + rotAxis.x * sin(angle),
+	cos(angle) + rotAxis.z * rotAxis.z * (1.0 - cos(angle))
+);
+	top = IN[i].worldPos + windRotation * (top - IN[i].worldPos);
 
-		vec3 cameraToVertex = IN[i].worldPos - cameraPosition;
-		vec3 viewDirection = normalize(cameraToVertex);
-		float frontFace = dot(viewDirection, IN[0].normal);
-		
-		if (frontFace < 0.0){
-			baseLeft = baseLeft + baseRight;
-			baseRight = baseLeft - baseRight;
-			baseLeft = baseLeft - baseRight;
-		}
-		
-		gl_Position = toClipSpace(baseRight);
-		OUT.colour = colourBladeBase; //colourBase;
-		EmitVertex();
-		
-		gl_Position = toClipSpace(baseLeft);
-		EmitVertex();
-
-		for (int i = 1; i < numSegments; ++i) {
-			float t = float(i) / float(numSegments);  // Interpolation factor
-			
-			float segmentHeight = height * t;
-			float segmentWidth = width * (1 - t);
-			
-			vec3 vertLeft = IN[i].worldPos - randomTangent * (segmentWidth / 2.0) + normalize(rotationYMatrix * rotationXMatrix * heightNormal) * segmentHeight;  
-			vec3 vertRight = IN[i].worldPos + randomTangent * (segmentWidth / 2.0) + normalize(rotationYMatrix * rotationXMatrix * heightNormal) * segmentHeight;
-			
-			vertLeft = IN[i].worldPos + t * windRotation * (vertLeft - IN[i].worldPos);
-			vertRight = IN[i].worldPos + t * windRotation * (vertRight - IN[i].worldPos);
-			OUT.colour = mix(colourBladeBase, colourTop, t); //mix(colourBase, colourTop, t);  // Interpolating color
-			
-			// Emit the segment vertex
-			gl_Position = toClipSpace(vertRight);
-			EmitVertex();
-			
-			gl_Position = toClipSpace(vertLeft);
-			EmitVertex();
-		}
-		
-		gl_Position = toClipSpace(top);
-		OUT.colour = colourBladeTop; //colourTop;
-		EmitVertex();
-		
-		EndPrimitive();
+	vec3 cameraToVertex = IN[i].worldPos - cameraPosition;
+	vec3 viewDirection = normalize(cameraToVertex);
+	float frontFace = dot(viewDirection, IN[0].normal);
+	
+	if (frontFace < 0.0){
+		baseLeft = baseLeft + baseRight;
+		baseRight = baseLeft - baseRight;
+		baseLeft = baseLeft - baseRight;
 	}
+	
+	gl_Position = toClipSpace(baseRight);
+	OUT.colour = colourBladeBase; //colourBase;
+	EmitVertex();
+	
+	gl_Position = toClipSpace(baseLeft);
+	EmitVertex();
+
+	for (int i = 1; i < numSegments; ++i) {
+		float t = float(i) / float(numSegments);  // Interpolation factor
+		
+		float segmentHeight = height * t;
+		float segmentWidth = width * (1 - t);
+		
+		vec3 vertLeft = IN[i].worldPos - randomTangent * (segmentWidth / 2.0) + normalize(rotationYMatrix * rotationXMatrix * heightNormal) * segmentHeight;  
+		vec3 vertRight = IN[i].worldPos + randomTangent * (segmentWidth / 2.0) + normalize(rotationYMatrix * rotationXMatrix * heightNormal) * segmentHeight;
+		
+		vertLeft = IN[i].worldPos + t * windRotation * (vertLeft - IN[i].worldPos);
+		vertRight = IN[i].worldPos + t * windRotation * (vertRight - IN[i].worldPos);
+		OUT.colour = mix(colourBladeBase, colourTop, t); //mix(colourBase, colourTop, t);  // Interpolating color
+		
+		// Emit the segment vertex
+		gl_Position = toClipSpace(vertRight);
+		EmitVertex();
+		
+		gl_Position = toClipSpace(vertLeft);
+		EmitVertex();
+	}
+	
+	gl_Position = toClipSpace(top);
+	OUT.colour = colourBladeTop; //colourTop;
+	EmitVertex();
+	
+	EndPrimitive();
 }
